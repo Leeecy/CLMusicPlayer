90ffdf6448bd4f3a184d83bcac96efb125f354ff
chenl_cl <347979248@qq.com>
2016-05-31 19:26:46 +0800

    第一次初始化

---
+++ CLMusicPlayer/CLMusicPlayer/ViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  CLMusicPlayer
+//
+//  Created by mac on 16/5/31.
+//  Copyright © 2016年 sunsmart. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+#pragma mark -
+#pragma mark 初始化UI
+-(void)confgiUI{
+    
+}
+#pragma mark -
+#pragma mark 加载数据
+
+#pragma mark -
+#pragma mark 事件
+
+#pragma mark -
+#pragma mark 数据请求
+
+#pragma mark -
+#pragma mark 代理
+
+#pragma mark -
+#pragma mark 业务逻辑
+
+#pragma mark -
+#pragma mark 通知注册和销毁
+
+@end
---
+++ CLMusicPlayer/CLMusicPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CLMusicPlayer
+//
+//  Created by mac on 16/5/31.
+//  Copyright © 2016年 sunsmart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
